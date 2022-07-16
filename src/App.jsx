1f4cd1efa56b8334189475a74db5ccf6b32676bf
sunday00 <kind65505256@gmail.com>
2022-07-16 22:23:19 +0900

    :tada: .
    .

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+import Home from '@v/Home.jsx';
+
+function App() {
+
+
+  return (
+    <div className="App" data-theme="dracula">
+      <Home />
+    </div>
+  )
+}
+
+export default App
